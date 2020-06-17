a2ba2f0b915a183641ee694749e5b34431762315
Emfine <601563839@qq.com>
2020-06-18 01:36:25 +0800

    UI设计

---
+++ src/CPOE/01.Clients/Desktop/Iguana.EHR.Controls.Forms/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iguana.EHR.Controls.Forms
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/CPOE/01.Clients/Desktop/Iguana.EHR/frmMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Iguana.EHR
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rcMain = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.defLook = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.rcMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rcMain
+            // 
+            this.rcMain.ApplicationIcon = global::Iguana.EHR.Properties.Resources.patient;
+            this.rcMain.ExpandCollapseItem.Id = 0;
+            this.rcMain.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.rcMain.ExpandCollapseItem});
+            this.rcMain.Location = new System.Drawing.Point(0, 0);
+            this.rcMain.MaxItemId = 2;
+            this.rcMain.Name = "rcMain";
+            this.rcMain.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.rcMain.ShowToolbarCustomizeItem = false;
+            this.rcMain.Size = new System.Drawing.Size(892, 151);
+            this.rcMain.Toolbar.ShowCustomizeItem = false;
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "ribbonPage1";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+            // 
+            // defLook
+            // 
+            this.defLook.LookAndFeel.SkinName = "Office 2013 Dark Gray";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 465);
+            this.Controls.Add(this.rcMain);
+            this.Name = "frmMain";
+            this.Ribbon = this.rcMain;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.rcMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl rcMain;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defLook;
+    }
+}
+
